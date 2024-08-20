225c18fcf651e6b7e04a66793dc682adc5611792
adrian-coronel <adrianonel78@gmail.com>
2024-08-20 18:40:10 -0500

    Final Commit

--- Demo01/Program.cs
+++ Demo01/Program.cs
@@ -6,23 +6,45 @@ namespace Demo01
     {
         static void Main(string[] args)
         {
-            Rectangulo rectangulo = new Rectangulo();
-            rectangulo.Base = 100;
-            rectangulo.Height = 200;
+            //Rectangulo rectangulo = new Rectangulo();
+            //rectangulo.Base = 100;
+            //rectangulo.Height = 200;
 
-            double area = rectangulo.GetArea();
-            double perimeter = rectangulo.GetPerimeter();
+            //double area = rectangulo.GetArea();
+            //double perimeter = rectangulo.GetPerimeter();
 
-            Console.WriteLine($"El área es {area}");
-            Console.WriteLine($"El perímetro es {perimeter}");
+            //Console.WriteLine($"El área es {area}");
+            //Console.WriteLine($"El perímetro es {perimeter}");
 
-            ////
-            
-            Person persona = new Person();    
-            persona.Firstname = "Adrian";
-            persona.FatherLastname = "Coronel";
-            persona.MotherLastname = "Mendoza";
-            persona.GetFullname();
+            //////
+
+            //Person persona = new Person();    
+            //persona.Firstname = "Adrian";
+            //persona.FatherLastname = "Coronel";
+            //persona.MotherLastname = "Mendoza";
+            //persona.GetFullname();
+
+
+            /////  
+
+            //Teacher teacher = new Teacher
+            //{
+            //    Firstname = persona.Firstname,
+            //    FatherLastname = persona.FatherLastname,
+            //    Salary = 2000,
+            //};
+
+
+            Rectangulo rectangulo = new Rectangulo
+            {
+                TopLeft = new Coordenadas { X = 0.0, Y = 8.0 },
+                TopRigth = new Coordenadas { X = 4.0, Y = 8.0 },
+                ButtomRigth = new Coordenadas { X = 0.0, Y = 0.0 },
+                ButtonLeft = new Coordenadas { X = 4.0, Y = 0.0 },
+            };
+
+            Console.WriteLine($"El área es {rectangulo.GetArea()}");
+            Console.WriteLine($"El perímetro es {rectangulo.GetPerimeter()}");
         }
     }
 }
--- Demo01/Rectangulo.cs
+++ Demo01/Rectangulo.cs
@@ -8,11 +8,40 @@ namespace Demo01
 {
     public class Rectangulo
     {
-        public double Base { get; set; }
-        public double Height { get; set; }
+        public Coordenadas TopRigth { get; set; }
+        public Coordenadas TopLeft { get; set; }
+        public Coordenadas ButtomRigth { get; set; }
+        public Coordenadas ButtonLeft { get; set; }
 
-        public double GetArea() { return Base * Height; }
 
-        public double GetPerimeter() { return 2 * (Base + Height); }
+        public double GetArea() { 
+            return GetBase() * GetAltura();
+        }
+        public double GetPerimeter()
+        {
+            return (GetBase() + GetAltura()) * 2;
+        }
+
+
+        public double GetBase()
+        {
+            return GetDistance(ButtonLeft, ButtomRigth);
+        }
+        public double GetAltura()
+        {
+            return GetDistance(TopRigth, ButtomRigth);
+        }
+        private double GetDistance(Coordenadas coord1, Coordenadas coord2)
+        {
+            return Math.Sqrt(Math.Pow(coord2.X - coord1.X, 2) + Math.Pow(coord2.Y - coord1.Y, 2));
+        }
+
+    }
+
+
+    public class Coordenadas
+    {
+        public double Y { get; set; }
+        public double X { get; set; }
     }
 }
